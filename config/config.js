03fea28da4ae082239ed0c849488ef1093ea8911
svenve-git <svenve@protonmail.com>
2020-10-12 12:05:58 +0200

    configure db connection w/ .env (ignored)

---
+++ config/config.js
@@ -0,0 +1,22 @@
+const DB_URL = require("dotenv")
+
+module.exports = {
+  development: {
+    url: DB_URL,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+}
